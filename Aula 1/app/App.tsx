b75d99a8036a0a35f9f8b5176ff8cda4517dc754
Eduardo Moraes <luis270704@gmail.com>
2024-08-12 22:28:54 -0300

    Aula 1 Completa

---
+++ Aula 1/app/App.tsx
@@ -0,0 +1,10 @@
+import { Cinco, Dois, Home, Quatro, Tres, Um } from "./src/screen";
+
+export default function App() {
+  return <Home />;
+  // return <Um />;
+  // return <Dois />;
+  // return <Tres />
+  // return <Quatro />;
+  // return <Cinco />;
+}
---
+++ Aula 1/app/src/screen/index.ts
@@ -0,0 +1,8 @@
+import Cinco from "./Cinco";
+import Dois from "./Dois";
+import Home from "./Home";
+import Quatro from "./Quatro";
+import Tres from "./Tres";
+import Um from "./Um";
+
+export { Home, Um, Dois, Tres, Quatro, Cinco };
